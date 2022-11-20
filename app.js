1c3e52f60c04dfd61a1b2ef635ad11a856a37bc3
Nelly <$>
2022-11-20 16:51:12 -0500

    updated the inputdigit function so thar way the numbers are displayed seperately after a operator button is clicked

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const calculator = {
   };
 
   function inputDigit(digit) {
-    const { displayValue } = calculator;
-    // Overwrite `displayValue` if the current value is '0' otherwise append to it
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  
+    console.log(calculator);
   }
 
   function inputDecimal(dot) {
@@ -17,6 +24,7 @@ const calculator = {
       // Append the decimal point
       calculator.displayValue += dot;
     }
+    console.log(calculator);
   }
 
   function handleOperator(nextOperator) {
