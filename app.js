f0abb6c12ac46c3d7b2bec9bd9456e637636af63
Nelly <$>
2022-11-20 16:46:33 -0500

    now able to add a decimal input and display it on the screen instead of the console

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ const calculator = {
     calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
 
+  function inputDecimal(dot) {
+    // If the `displayValue` property does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+      calculator.displayValue += dot;
+    }
+  }
+
+  function handleOperator(nextOperator) {
+    // Destructure the properties on the calculator object
+    const { firstOperand, displayValue, operator } = calculator
+    // `parseFloat` converts the string contents of `displayValue`
+    // to a floating-point number
+    const inputValue = parseFloat(displayValue);
+  
+    // verify that `firstOperand` is null and that the `inputValue`
+    // is not a `NaN` value
+    if (firstOperand === null && !isNaN(inputValue)) {
+      // Update the firstOperand property
+      calculator.firstOperand = inputValue;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+
   function updateDisplay() {
     // select the element with class of `calculator-screen`
     const display = document.querySelector('.calculator-screen');
@@ -32,12 +58,14 @@ keys.addEventListener('click', (event) => {
   }
 
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
+    handleOperator(target.value);
+    updateDisplay();
     return;
   }
 
   if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
+    inputDecimal(target.value);
+    updateDisplay();
     return;
   }
 
